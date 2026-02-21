006f9b6c788f0518327c59029b95afa51bd09105
AndresOrejarena <aorejarena@unimagdalena.edu.co>
2026-02-20 20:03:49 -0500

    Feat: Added corner correction and new collision system for both vertical and horizontal collisions. Mental Note: I dont want to touch again the collisions.

--- js/main.js
+++ js/main.js
@@ -17,23 +17,35 @@ fetch("json/map1.json")
         platforms.push(...data.platforms);
     });
 
+const colorList = ["orange", "blue", "green", "purple"];
 
 const player = {
     x: 100,
     y: 0,
-    width: 50,
-    height: 50,
-    speed: 5,
+    width: 30,
+    height: 40,
+    speed: 2,
     dx: 0,
     dy: 0,
     gravity: 1,
     jump_force: 24,
     onGround: true,
+    blockColor: "white"
 };
 
 const camera = {
     x: 0,
     y: 0
+};
+
+const hitboxOffsetX = 15;
+const hitboxWidth = player.width - 2 * hitboxOffsetX;
+
+const hitbox = {
+    x: player.x + hitboxOffsetX,
+    y: player.y,
+    width: hitboxWidth,
+    height: player.height
 };
 
 const keys = {};
@@ -49,11 +61,12 @@ document.addEventListener("keyup", (e) => {
 });
 
 
+
 function update() {
 
-    let previousY = player.y;
+    previousY = player.y;
+    previousX = player.x;
 
-    // INPUT HORIZONTAL
     if (keys["ArrowRight"]) {
         player.dx = player.speed;
     } else if (keys["ArrowLeft"]) {
@@ -62,47 +75,92 @@ function update() {
         player.dx = 0;
     }
 
-    // APLICAR GRAVEDAD
+    //Gravedad
     player.dy += player.gravity;
 
-    // MOVER
+    //Mover
     player.y += player.dy;
     player.x += player.dx;
 
-    // LIMITES HORIZONTALES
+
+
+    //Limites horizontales
     if (player.x < 0) player.x = 0;
     if (player.x + player.width > worldWidth)
         player.x = worldWidth - player.width;
 
-    // RESET GROUND
+    //Ground reset
     player.onGround = false;
 
-    // COLISIONES
+    //Colisiones
     platforms.forEach(platform => {
+        if (platform.collision.type === "solid") {
 
-        const isHorizontallyColliding =
-            player.x < platform.x + platform.width &&
-            player.x + player.width > platform.x;
+            const isVerticallyColliding =
+                player.y < platform.y + platform.height &&
+                player.y + player.height > platform.y;
 
-        if (!isHorizontallyColliding) return;
+            const isHorizontallyColliding =
+                player.x < platform.x + platform.width &&
+                player.x + player.width > platform.x;
 
-        if (
-            player.dy >= 0 &&
-            previousY + player.height <= platform.y &&
-            player.y + player.height >= platform.y
-        ) {
-            player.y = platform.y - player.height;
-            player.dy = 0;
-            player.onGround = true;
+            if (isVerticallyColliding) {
+                if (
+                    player.dx >= 0 &&
+                    previousX + player.width >= platform.x &&
+                    previousX + player.width - player.dx < platform.x
+                ) {
+                    player.dx = 0
+                    player.x = platform.x - player.width;
+                }
+                if (
+                    player.dx <= 0 &&
+                    previousX <= platform.x + platform.width &&
+                    previousX - player.dx > platform.x + platform.width
+                ) {
+                    player.dx = 0
+                    player.x = platform.x + platform.width;
+                }
+            }
+
+
+            if (isHorizontallyColliding) {
+                if (
+                    player.dy >= 0 &&
+                    previousY + player.height >= platform.y &&
+                    previousY + player.height - player.dy < platform.y
+                ) {
+                    player.y = platform.y - player.height;
+                    player.dy = 0;
+                    player.onGround = true;
+                }
+                if (
+                    player.dy <= 0 &&
+                    player.y <= platform.y + platform.height &&
+                    player.y - player.dy > platform.y + platform.height
+                ) {
+                    if (platform.x + platform.width -player.x  <= player.width*0.25) {
+                        player.x = platform.x + platform.width;
+                        return;
+                    }
+                    if (player.x + player.width - platform.x <= player.width*0.25){
+                        player.x = platform.x - player.width;
+                        return;
+                    }
+                    player.y = platform.y + platform.height;
+                    player.dy = 0;
+                }
+            }
         }
-    });
 
-    // SALTO (DESPUÉS de saber si está en el suelo)
+    });
+    //Salto (Solo si está en el suelo)
     if (keys["ArrowUp"] && player.onGround) {
         player.dy = -player.jump_force;
         player.onGround = false;
     }
 
+    //Actualizacion de la posicion de la camara
     camera.x = Math.max(
         0,
         Math.min(worldWidth - canvas.width, player.x - canvas.width / 2)
@@ -111,8 +169,7 @@ function update() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Jugador
-    ctx.fillStyle = "cyan";
+    ctx.fillStyle = player.blockColor;
     ctx.fillRect(
         player.x - camera.x,
         player.y - camera.y,
@@ -120,7 +177,7 @@ function draw() {
         player.height
     );
 
-
+    ctx.fillStyle = "blue";
     platforms.forEach(platform => {
         ctx.fillRect(
             platform.x - camera.x,
